dfd22f4a95cfd5073e642e440dee362e82e2b23b
Olavo Adriel <ollavoadriel@gmail.com>
2026-01-30 13:46:35 -0300

    Adicionado so teste automatizados, exemplo pratico

---
+++ models/calculadora.js
@@ -0,0 +1,5 @@
+function somar(argument1, argument2) {
+  return argument1 + argument2;
+}
+
+exports.somar = somar;
---
+++ tests/calculadora.test.js
@@ -0,0 +1,6 @@
+const calculadora = require('../models/calculadora.js');
+
+test("Deve retornar 8 ao somar 4 e 4", () => {
+  const resultado = calculadora.somar(4, 4);
+  expect(resultado).toBe(8);
+});
