e08a3c7a5102755076832e84310c342e90544f16
Olavo Adriel <ollavoadriel@gmail.com>
2026-01-30 17:04:34 -0300

    adiciona teste unitário para a função de soma da calculadora

--- tests/unit/calculadora.test.js
+++ tests/unit/calculadora.test.js
@@ -1,4 +1,4 @@
-const calculadora = require('../models/calculadora.js');
+const calculadora = require('../../models/calculadora.js');
 
 test("Deve retornar 8 ao somar 4 e 4", () => {
   const resultado = calculadora.somar(4, 4);
