4d353b25bd29ba7cd4d0c4726a6c3f418802a209
Olavo Adriel <110116287+olavuwdev@users.noreply.github.com>
2026-02-03 04:14:36 +0000

    Atualiza endpoint de status para retornar informações detalhadas do banco de dados

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;');
-  console.log(result.rows);
-  response.status(200).json({ status: 'ok' });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersion.rows[0].server_version;
+
+  const databaseMaxConnectionResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue = databaseMaxConnectionResult.rows[0].max_connections
+
+  const databaseOpennedConnections = 
+  await database.query("SELECT count(*)::int FROM pg_stat_activity where datname = 'local_db' ;");
+  const databaseOpennedConnectionsValue = databaseOpennedConnections.rows[0].count;
+  console.log(databaseOpennedConnectionsValue)
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies:{
+      database:{
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        oppened_connections: databaseOpennedConnectionsValue
+      }
+    }
+  });
 }
 
 export default status;
