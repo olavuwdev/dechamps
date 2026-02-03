90bc23dbcf266edbc99f3f589bad4c5bc87c8668
Olavuwdev <ollavoadrie@gmail.com>
2026-02-03 09:27:31 -0300

    Finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,23 +3,30 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
+  const databaseName = process.env.POSTGRES_DB;
+
   const databaseVersion = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersion.rows[0].server_version;
 
   const databaseMaxConnectionResult = await database.query("SHOW max_connections;");
   const databaseMaxConnectionsValue = databaseMaxConnectionResult.rows[0].max_connections
 
-  const databaseOpennedConnections = 
-  await database.query("SELECT count(*)::int FROM pg_stat_activity where datname = 'local_db' ;");
+  const databaseOpennedConnections =
+    await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity where datname = $1;",
+      values: [databaseName]
+    });
   const databaseOpennedConnectionsValue = databaseOpennedConnections.rows[0].count;
   console.log(databaseOpennedConnectionsValue)
+
+
   response.status(200).json({
     updated_at: updatedAt,
-    dependencies:{
-      database:{
+    dependencies: {
+      database: {
         version: databaseVersionValue,
         max_connections: parseInt(databaseMaxConnectionsValue),
-        oppened_connections: databaseOpennedConnectionsValue
+        oppened_connections: parseInt(databaseOpennedConnectionsValue)
       }
     }
   });
