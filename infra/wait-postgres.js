3857ae90ead255202fda42d038dd5c416ebb5fa2
Olavuwdev <ollavoadrie@gmail.com>
2026-02-20 13:24:40 -0300

    adds  script

---
+++ infra/wait-postgres.js
@@ -0,0 +1,18 @@
+const { exec } = require("node:child_process");
+
+
+function checkPostgres() {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
+  function handleReturn(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write("🟩");
+      checkPostgres();
+      return;
+    }
+    console.log("\n🟢 PostgreSQL está pronto para uso! \n");
+  }
+}
+
+process.stdout.write("🔴 Aguardando a execução do PostgreSQL.")
+checkPostgres();
