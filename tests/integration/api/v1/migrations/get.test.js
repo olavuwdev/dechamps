01abe5f8910f8954621b1a7062b25c50858e3277
Olavuwdev <ollavoadrie@gmail.com>
2026-02-20 15:41:33 -0300

    makes > galego-fera@1.0.0 test > npm run services:up && npm run wait-postgres && concurrently --n next,jest --hide next -k -s command-jest 'sleep 5; next dev' 'jest --runInBand'
    > galego-fera@1.0.0 services:up
    > docker compose -f infra/compose.yaml up -d
    
    > galego-fera@1.0.0 wait-postgres
    > node infra/wait-postgres.js
    
    🔴 Aguardando a execução do PostgreSQL.
    🟢 PostgreSQL está pronto para uso!
    
    [jest] PASS tests/integration/api/v1/migrations/post.test.js (8.775 s)
    [jest] PASS tests/integration/api/v1/status/get.test.js
    [jest] PASS tests/integration/api/v1/migrations/get.test.js
    [jest]
    [jest] Test Suites: 3 passed, 3 total
    [jest] Tests:       1 skipped, 3 passed, 4 total
    [jest] Snapshots:   0 total
    [jest] Time:        9.298 s, estimated 11 s
    [jest] Ran all test suites.
    [jest] jest --runInBand exited with code 0
    --> Sending SIGTERM to other processes.. more  robust with  and

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,9 +1,11 @@
 import database from "infra/database";
-beforeAll(cleanDatabase); //Antes de Tudo rode a função cleanDatabase para limpar o banco de dados
-
-async function cleanDatabase() {
+import orchestractor from "tests/orchestractor";
+beforeAll(async () => {
+  await orchestractor.waitForAllProcess();
   await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
-}
+}); //Antes de Tudo rode a função cleanDatabase para limpar o banco de dados
+
+
 
 test("Get to api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
