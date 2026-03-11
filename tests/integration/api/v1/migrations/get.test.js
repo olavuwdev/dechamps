96508b4f923cf40415b2039be74d83e87986978a
Olavo Adriel <110116287+olavuwdev@users.noreply.github.com>
2026-03-11 23:32:43 +0000

    refactor: ajust formating code

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,14 +1,12 @@
-import database from "infra/database";
 import orchestractor from "tests/orchestractor";
 beforeAll(async () => {
   await orchestractor.waitForAllProcess();
   await orchestractor.cleanDatabaseProcess();
 }); //Antes de Tudo rode a função cleanDatabase para limpar o banco de dados
 
-
-describe("GET '/api/v1/migrations' ", () => {
+describe("GET '/api/v1/migrations'", () => {
   describe("Anonymous user", () => {
-    test("Runnnig pending migrations ", async () => {
+    test("Runnnig pending migrations", async () => {
       const response = await fetch("http://localhost:3000/api/v1/migrations");
       expect(response.status).toBe(200);
 
