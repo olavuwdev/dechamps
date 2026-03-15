794f784e2c7dc074c598566047842431b21fd442
Olavo Adriel <ollavoadriel@gmail.com>
2026-03-14 21:57:55 -0300

    file teste.js

---
+++ teste.js
@@ -0,0 +1,40 @@
+class ValitationError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 400;
+  }
+}
+
+function salvarUsuario(input) {
+
+  if (!input) {
+    throw new ReferenceError("é necessário fornecer um objeto de entrada.");
+  }
+  if (!input.name) {
+    throw new ValitationError("Preencha o campo de nome.");
+  }
+  if (!input.username) {
+    throw new ValitationError("Preencha o campo de nome de usuário.");
+  }
+  if (!input.age) {
+    throw new ValitationError("Preencha o campo de idade.");
+  }
+
+  user.save(input);
+
+}
+try {
+  salvarUsuario({
+    name: "Olavo Adriel",
+  });
+} catch (error) {
+  if (error instanceof ReferenceError) {
+    throw error;
+  }
+  if (error instanceof ValitationError) {
+    console.log(error);
+    return; // response.status(400).json({ error: error.message });
+  }
+  console.error("Ocorreu um erro ao salvar o usuário:");
+  console.error(error);
+}
