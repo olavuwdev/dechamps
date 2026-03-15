742478eaa346222e6aa9da36f59d047771cff793
Olavo Adriel <110116287+olavuwdev@users.noreply.github.com>
2026-03-15 10:54:36 +0000

    feat: add custom errors

---
+++ infra/erros.js
@@ -0,0 +1,22 @@
+export class InternalServerError extends Error{
+    constructor({cause}){
+        super("Um erro interno não esperado aconteceu!", {
+            cause
+        })
+
+        this.name = "InternalServerError";
+        this.action = "Entre em contato com o suporte.";
+        this.statusCode = 500;
+
+    }
+    toJSON(){
+        return{
+            name: this.message,
+            message: this.message,
+            action: this.action,
+            status_code: this.statusCode
+        }
+    }
+    
+
+}
