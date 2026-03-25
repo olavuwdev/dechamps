602e262c95fede59021e8763c2a81c1dcf51fcec
Olavo Adriel <110116287+olavuwdev@users.noreply.github.com>
2026-03-25 01:49:05 +0000

    feat: create new error `NotFoundError`

--- infra/erros.js
+++ infra/erros.js
@@ -36,6 +36,44 @@ export class ServiceError extends Error {
     };
   }
 }
+export class ValidationError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Erro de validação ocorreu.", {
+      cause,
+    });
+
+    this.name = "ValidationError";
+    this.action = action || "Ajuste os dados enviados e tente novamente.";
+    this.statusCode = 400;
+  }
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Não foi possivel encontrar esse recurso no sistema.", {
+      cause,
+    });
+
+    this.name = "NotFoundError";
+    this.action = action || "Verifique os dados enviados e tente novamente.";
+    this.statusCode = 404;
+  }
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
 export class MethodNotAllowedError extends Error {
   constructor() {
     super("Método não permitido para este endpoint.");
